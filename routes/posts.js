9493f75d44e2e2c757b63055b825be335e898def
gabriel.joao61@aluno.ifce.edu.br <kaua.sousa63@aluno.ifce.edu.br>
2025-10-21 17:35:19 -0300

    Consertando erros de sintaxe: Posts - posts

--- routes/posts.js
+++ routes/posts.js
@@ -3,19 +3,19 @@ const router = express.Router();
 const {posts} = require("../models");
 
 router.get("/", async (req, res) => {
-    const listOfPosts = await Posts.findAll();
+    const listOfPosts = await posts.findAll();
     res.json(listOfPosts);
 });
 
 router.get("/:id", async (req, res) => {
     const id = req.params.id;
-    const post = await Posts.findByPk(id);
+    const post = await posts.findByPk(id);
     res.json(post);
 });
 
 router.post("/", async (req, res) => {
     const post = req.body;
-    await Posts.create(post);
+    await posts.create(post);
     res.json(post);
 });
 
